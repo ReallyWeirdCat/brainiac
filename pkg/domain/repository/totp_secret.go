332d2e6d3b102c02657263f262f2adae191a06ad
Nikolai Papin <nikolai@weirdcat.ru>
2026-06-12 14:12:50 +0300

    feat: totp secret

---
+++ pkg/domain/repository/totp_secret.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2026 Nikolai Papin
+//
+// This file is part of Brainiac gamification and education platform
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package repository
+
+import (
+	"context"
+
+	"github.com/ReallyWeirdCat/brainiac/pkg/domain/entity"
+	"github.com/ReallyWeirdCat/brainiac/pkg/domain/valueobject"
+)
+
+type TOTPSecretRepository interface {
+	Save(ctx context.Context, user entity.TOTPSecret) error
+	Delete(ctx context.Context, guid valueobject.GUID) error
+	GetByGUID(ctx context.Context, guid valueobject.GUID) (*entity.TOTPSecret, error)
+	GetByUsername(ctx context.Context, username string) (*entity.TOTPSecret, error)
+	ExistsByUsername(ctx context.Context, username string) (bool, error)
+}
